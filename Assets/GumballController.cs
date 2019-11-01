152348d85202b702e816cb668b26da5f6d443890
michikogo <37466954+michikogo@users.noreply.github.com>
2019-11-01 17:18:38 +0800

    delay added when starting

--- Assets/GumballController.cs
+++ Assets/GumballController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GumballController : MonoBehaviour
 {
     [SerializeField] private float speed = 1.0f; // speed in meters per second
-    private float smooth = 5.0f;
+    private float time = 1.0f;
     private int score = 0;
     private bool left = false;
     private bool right = false;
@@ -21,46 +21,56 @@ public class GumballController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (time >= 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-        // Right
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            this.right = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            this.right = false;
-        }
-        if (this.right)
-        {
-            Vector3 pos2 = this.transform.localPosition;
-            pos2.x += (Time.deltaTime * this.speed);
-            this.transform.localPosition = pos2;
-            // Debug.Log("Right");
+            // Right
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                this.right = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.D))
+            {
+                this.right = false;
+            }
+            if (this.right)
+            {
+                Vector3 pos2 = this.transform.localPosition;
+                pos2.x += (Time.deltaTime * this.speed);
+                this.transform.localPosition = pos2;
+                // Debug.Log("Right");
+            }
+
+            // Left
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                this.left = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.A))
+            {
+                this.left = false;
+            }
+            if (this.left)
+            {
+                Vector3 pos3 = this.transform.localPosition;
+                pos3.x -= (Time.deltaTime * this.speed);
+                this.transform.localPosition = pos3;
+                // Debug.Log("Left");
+            }
         }
 
-        // Left
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            this.left = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.A))
-        {
-            this.left = false;
-        }   
-        if (this.left)
-        {
-            Vector3 pos3 = this.transform.localPosition;
-            pos3.x -= (Time.deltaTime * this.speed);
-            this.transform.localPosition = pos3;
-            // Debug.Log("Left");
-        }
-
+        // Exiting the Application
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
+
+
     }
     
     void OnCollisionEnter(Collision collision)
@@ -80,5 +90,13 @@ public class GumballController : MonoBehaviour
             Debug.Log("I am inside the portal");
             LevelManager.Instance.loadLevel();
         }
+    }
+
+    // Delay player movement
+    IEnumerator Example()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(5);
+        print(Time.time);
     }
 }
