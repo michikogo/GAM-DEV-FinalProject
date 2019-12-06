2c98b348d1e82960b47a3212ad6497c66fd9d391
michikogo <37466954+michikogo@users.noreply.github.com>
2019-12-07 03:26:14 +0800

    changed level names

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelManager
 {
     private static LevelManager sharedInstance = null;
-    private string currentScene = "Level5";
+    private string currentScene = "Level1";
     //private string MENU = "SceneMenu";
     // Level 1-10
     private string LEVEL_ONE = "Level1";
@@ -16,6 +16,11 @@ public class LevelManager
     private string LEVEL_FIVE = "Level5";
     private string LEVEL_SIX = "Level6";
     private string LEVEL_SEVEN = "Level7";
+    private string LEVEL_EIGHT = "Level8";
+    private string LEVEL_NINE = "Level9";
+    private string LEVEL_TEN = "Level10";
+    // Emission
+    private string LEVEL_ELEVEN = "Level11";
 
     private LevelManager()
     {
@@ -58,6 +63,15 @@ public class LevelManager
             this.currentScene = LEVEL_SIX;
         else if (this.currentScene == LEVEL_SIX)
             this.currentScene = LEVEL_SEVEN;
+        else if (this.currentScene == LEVEL_SEVEN)
+            this.currentScene = LEVEL_EIGHT;
+        else if (this.currentScene == LEVEL_EIGHT)
+            this.currentScene = LEVEL_NINE;
+        else if (this.currentScene == LEVEL_NINE)
+            this.currentScene = LEVEL_TEN;
+        // Emission
+        else if (this.currentScene == LEVEL_TEN)
+            this.currentScene = LEVEL_ELEVEN;
 
         Debug.Log(this.currentScene);
         SceneManager.LoadScene(this.currentScene);
